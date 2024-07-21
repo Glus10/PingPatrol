74c011392b14e18d81455d7f4c7230eaadf0b5f9
Glus10 <yair@gluska.com>
2024-07-21 17:13:06 +0300

    Initial commit

---
+++ src/main/java/com/example/PingPatrol/PingPatrolApplication.java
@@ -0,0 +1,13 @@
+package com.example.PingPatrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingPatrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingPatrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PingPatrol/PingPatrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PingPatrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingPatrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
